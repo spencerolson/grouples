f5a7883e3f1d9b0b2bb798a2e73b7acb2ecbed4b
Tyler Keating & Spencer Olson <pair+bontgoy.spencerolson@devbootcamp.com>
2014-07-17 18:51:57 -0500

    add coveralls info

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,8 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'database_cleaner'
 require 'shoulda/matchers'
-# require 'coveralls'
-# Coveralls.wear!('rails')
+require 'coveralls'
+Coveralls.wear!('rails')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
