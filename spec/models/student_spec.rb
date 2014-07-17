c1629adf9d114f4c0ec17720ea25128c43ceea10
Tyler Keating & Spencer Olson <pair+bontgoy.spencerolson@devbootcamp.com>
2014-07-17 16:12:22 -0500

    started adding drag drop functionality

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Student, :type => :model do
   it { should validate_presence_of(:name) }
 
   it { should have_many(:student_groups) }
-  it { should have_many(:groups) }
+  it { should have_many(:groups).through(:student_groups) }
   it { should have_one(:photo) }
   it { should belong_to(:cohort) }
 end
