671c99373e4174298dc3b38156c2324fc77d1aba
Matthew Hein <mvernhein@gmail.com>
2014-07-17 11:37:21 -0500

    access restricted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   include Monban::ControllerHelpers
-	include Monban::ControllerHelpers
+  before_action :require_login
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def require_login
+    unless signed_in?
+      flash[:error] = "You must be logged in to acccess this section"
+      redirect_to new_session_path
+    end
+  end
 end
