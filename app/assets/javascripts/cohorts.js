f04066411e08b8044d9fe5c8b2c06cf175a44e04
bontgoy <tyjkeating@gmail.com>
2014-07-17 18:55:23 -0500

    merge test

---
+++ app/assets/javascripts/cohorts.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+
+    $(".makeMeDraggable").draggable({
+    	snap: '.makeMeDroppable'
+    });
+
+    $(".makeMeDroppable").droppable({
+      drop: handleDropEvent,
+    });
+
+
+  function handleDropEvent( event, ui ) {
+    var draggable = ui.draggable;
+    alert("Who loves pizza!!!")
+  }
+});
