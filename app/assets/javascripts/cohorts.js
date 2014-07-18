7e674a3f9751c96184eccae2042e7382404938fa
bontgoy <tyjkeating@gmail.com>
2014-07-17 19:00:22 -0500

    got rid of alert in js

--- app/assets/javascripts/cohorts.js
+++ app/assets/javascripts/cohorts.js
@@ -1,28 +1,22 @@
-<<<<<<< HEAD
+
 
 $(document).ready(function() {
 
     $(".makeMeDraggable").draggable({
-=======
+
 $(document).ready(function() {
 
     $(".makeMeDraggable").draggable({
       revert: "invalid",
->>>>>>> master
+
     	snap: '.makeMeDroppable'
     });
 
     $(".makeMeDroppable").droppable({
       drop: handleDropEvent,
     });
-
-
+    
   function handleDropEvent( event, ui ) {
     var draggable = ui.draggable;
-<<<<<<< HEAD
-    alert("Who loves pizza!!!")
-=======
-
->>>>>>> master
   }
 });
